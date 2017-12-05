d66e9f0d6652777ed86e83291bf61b139a174a5a
Anton Lashchotka <anton_lashchotka@epam.com>
2017-12-05 21:46:03 +0300

    add cost

--- client/app/features/Profile/Profile.js
+++ client/app/features/Profile/Profile.js
@@ -3,14 +3,26 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import PropTypes from 'prop-types';
+import { isEmpty, forEach } from 'lodash';
 
 import { TabNavigator } from '../../features';
 import { styles } from './styles';
 
 class Profile extends Component {
+  state = {
+    cost: 0
+  }
+
   componentDidMount() {
+    const { token, basket } = this.props;
     if (!this.props.token) {
       Actions.authorization();
+    } else {
+      let cost = 0;
+      forEach(basket, (item) => cost += item.price);
+      this.setState({
+        cost
+      })
     }
   }
 
@@ -20,25 +32,49 @@ class Profile extends Component {
   }
 
   createBasket = () => {
+    const { basket } = this.props;
+    if (isEmpty(basket)) {
+      return (
+        <View style={styles.empty}>
+          <Text style={styles.title}>Basket</Text>
+          <Text style={styles.description}>Basket is empty</Text>
+          <TouchableOpacity
+            style={styles.buttonGoToCatalogue}
+            onPress={this.onPress}
+          >
+            <Text style={styles.buttonText}>Go to Shopping!</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
     return (
-      <View style={styles.empty}>
-        <Text style={styles.title}>Basket</Text>
-        <Text style={styles.description}>Basket is empty</Text>
-        <TouchableOpacity
-          style={styles.buttonGoToCatalogue}
-          onPress={this.onPress}
-        >
-          <Text style={styles.buttonText}>Go to Shopping!</Text>
-        </TouchableOpacity>
+      <View style={styles.favourites}>
+        <ClothingsRow clothings={favourites} />
       </View>
     );
   }
 
+  createBuyButton = () => {
+    const { basket } = this.props;
+    if (isEmpty(basket)) {
+      return (
+        <TouchableOpacity
+          style={styles.buyButton}
+        >
+          <Text style={styles.buyButtonText}>Cost: {this.state.cost}</Text>
+        </TouchableOpacity>
+      )
+    }
+    return null;
+  }
+
   render() {
     const basket = this.createBasket();
+    const buyButton = this.createBuyButton();
     return (
       <View style={styles.container}>
         {basket}
+        {buyButton}
         <TabNavigator />
       </View>
     );
@@ -50,7 +86,8 @@ Profile.propTypes = {
 };
 
 const mapStateToProps = state => ({
-  token: state.user.token
+  token: state.user.token,
+  basket: state.user.basket
 });
 
 export default connect(mapStateToProps)(Profile);
